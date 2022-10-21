7d49485219bbb6fe6e49c6ce2934a48bb417e0a0
FREDYKRUGE <chrismas10@abv.bg>
2022-10-21 11:50:23 +0300

    update

--- ExamPrep2/03. Company-administration/tests.js
+++ ExamPrep2/03. Company-administration/tests.js
@@ -1,1 +1,14 @@
-//TODO
+let companyAdministration = require('./companyAdministration')
+let { assert } = require('chai');
+
+describe("Tests …", function () {
+    describe("test hiringEmployee", function () {
+         let hiringEmployee = companyAdministration.hiringEmployee
+        it("Test different position", function () {
+            let exp = `We are not looking for workers for this position.`
+            assert.throw(() => hiringEmployee('Todor', 'DifferentPosition', 30),exp )
+        });
+    });
+
+    // TODO: …
+});
